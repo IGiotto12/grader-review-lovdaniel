0dfe6bd5f106dba7ef63c9e107812c3fea521a3e
Daniel Lov <83891229+lov-daniel@users.noreply.github.com>
2024-03-06 12:23:22 -0800

    upload

---
+++ grade.sh
@@ -0,0 +1,41 @@
+CPATH='.;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar'
+rm -rf student-submission
+rm -rf grading-area
+mkdir grading-area
+git clone $1 student-submission
+echo 'Finished cloning'
+if [ -f student-submission/ListExamples.java ]
+then
+    cp student-submission/*.java grading-area
+    cp TestListExamples.java grading-area
+    cp -r lib grading-area
+else
+    echo "Missing student-submission/ListExamples.java, did you forget the file or misname it?"
+    echo "Score: 0"
+    exit 1
+fi
+cd grading-area
+javac -cp $CPATH *.java &> error-output.txt
+
+if [ $? -ne 0 ]
+then
+    echo "Compilation Error"
+    echo "Score:0"
+    exit 1
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > junit-output.txt
+
+lastline=$(cat junit-output.txt | tail -n 2 | head -n 1)
+check=$(echo $lastline | cut -c1-2)
+
+if [ "$check" = "OK" ]
+then
+    tests=$(echo $lastline | awk -F'[( ]' '{print $3}')
+    successes=$(echo $lastline | awk -F'[( ]' '{print $3}')
+else
+    tests=$(echo $lastline | awk -F'[, ]' '{print $3}')
+    failures=$(echo $lastline | awk -F'[, ]' '{print $6}')
+    successes=$((tests - failures))
+fi
+echo "Your score is $successes / $tests"
